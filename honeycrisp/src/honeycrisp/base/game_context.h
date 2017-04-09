3fc649c09b0d0c73fa6fb874abe5fdc0e6132d6c
Matt Westerburg <matt.westerburg@bestbuy.com>
2017-04-09 13:03:23 -0500

    Updated the game context

--- honeycrisp/src/honeycrisp/base/game_context.h
+++ honeycrisp/src/honeycrisp/base/game_context.h
@@ -1,11 +1,21 @@
 #pragma once
 
+#include "../controllers/keyboard.h"
+#include "../controllers/gamepad.h"
+#include "sdl.h"
 #include "macros.h"
+#include <vector>
 
 NS_HC_BEGIN
 
+using std::vector;
+
 namespace Base {
-struct hcGameContext_t {};
+struct hcGameContext_t {
+  vector<Controllers::Gamepad *> gamepads;
+  Controllers::Keyboard *keyboard;
+  RendererSPtr renderer;  
+};
 
 using hcGameContext = hcGameContext_t;
 } // namespace Base
