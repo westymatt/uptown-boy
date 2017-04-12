50f18328199ffd6d3f6faf0948610afbf772b27d
Matt Westerburg <matt.westerburg@bestbuy.com>
2017-04-11 22:20:29 -0500

    Game header clean up

--- honeycrisp/src/honeycrisp/base/game.h
+++ honeycrisp/src/honeycrisp/base/game.h
@@ -1,28 +1,12 @@
 #pragma once
 
 #include "../2d/scene.h"
-#include "../2d/sprite.h"
 #include "../base/game_config.h"
 #include "../base/macros.h"
-#include "../base/sdl.h"
-#include "../controllers/keyboard.h"
-#include "../utils/logging.h"
 #include "../base/timer.h"
 #include "game_context.h"
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <iostream>
-#include <map>
-#include <memory>
-#include <string>
-#include <vector>
 
 NS_HC_BEGIN
-
-using std::unique_ptr;
-using std::map;
-using std::string;
-using std::vector;
 
 class hcGame {
 public:
